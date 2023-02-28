7c61f938d5d04c18a0dd12b1402e6b7f2624352e
yve-n <yve.nguetche@gmail.com>
2023-02-28 23:58:44 +0100

    setting up the first structure of the project

---
+++ src/main/java/com/cda/freely/FreelyApplication.java
@@ -0,0 +1,13 @@
+package com.cda.freely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cda/freely/FreelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cda.freely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
