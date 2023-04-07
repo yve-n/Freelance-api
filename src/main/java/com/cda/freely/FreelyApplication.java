88dcc4488a472d321a5f428027be952a384c1a1e
yve-n <yve.nguetche@gmail.com>
2023-04-07 14:04:07 +0200

    added a method for the application to take the system time at startup

--- src/main/java/com/cda/freely/FreelyApplication.java
+++ src/main/java/com/cda/freely/FreelyApplication.java
@@ -1,13 +1,26 @@
 package com.cda.freely;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class FreelyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FreelyApplication.class, args);
+	}
+
+	/**
+	 * @PostConstruct lance la méthode après l'initialisation de spring.
+	 * @init() cette méthode définit le fuseau horaire à UTC afin que l'application 
+	 * s'adapte à l'heure du système
+	 * */
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
