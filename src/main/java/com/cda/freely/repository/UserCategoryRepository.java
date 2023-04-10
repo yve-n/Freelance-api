3a25ae358e6f8939971ae17faa4f7f8659b75141
yve-n <yve.nguetche@gmail.com>
2023-04-10 04:17:20 +0200

    add a new join table for user and category

--- src/main/java/com/cda/freely/repository/UserCategoryRepository.java
+++ src/main/java/com/cda/freely/repository/UserCategoryRepository.java
@@ -1,4 +1,12 @@
 package com.cda.freely.repository;
 
-public interface UserCategoryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.cda.freely.entity.UserCategory, java.lang.Integer> {
+import com.cda.freely.entity.UserCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserCategoryRepository extends JpaRepository<UserCategory, UserCategory.KeyUserCategory> {
+    Optional<UserCategory> findByUserCategoryId_IdUserAndUserCategoryId_IdCategory(Integer id_user , Integer id_category);
 }
