4294b43b5bc3a5917e828782dbe00c1164fc300c
yve-n <yve.nguetche@gmail.com>
2023-05-02 16:52:03 +0200

    add cors configuration

--- src/main/java/com/cda/freely/config/SecurityConfig.java
+++ src/main/java/com/cda/freely/config/SecurityConfig.java
@@ -18,7 +18,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -43,6 +47,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
@@ -86,5 +92,24 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager() {
         return new ProviderManager(Collections.singletonList(authenticationProvider()));
     }
+
+    /**
+     *  Configuration des CORS(Cross Origin Resource Sharing)
+     *  autorisant uniquement les origines, les méthodes et les en-têtes spécifiques dont on a spécifié.
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Autorise toutes les origines ou spécifiez une liste d'origines autorisées
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Autorise les méthodes spécifiées
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Autorise tous les en-têtes ou spécifiez une liste d'en-têtes autorisés
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Disposition","Content-Type","Origin", "X-Request-with", "x-access-token","Content","Accept")); // Expose les en-têtes spécifiés
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
