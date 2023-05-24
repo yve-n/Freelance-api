ad0462bbcdebec2e738c9a0f42163ce00df90596
yve-n <yve.nguetche@gmail.com>
2023-05-24 23:56:18 +0200

    maj

--- src/main/java/com/cda/freely/repository/UserRepository.java
+++ src/main/java/com/cda/freely/repository/UserRepository.java
@@ -2,10 +2,14 @@ package com.cda.freely.repository;
 
 import com.cda.freely.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u JOIN FETCH u.tags WHERE u.id = :id")
+    Optional<User> findByIdWithTags(@Param("id") Long id);
     User findByEmail(String email);
 }
