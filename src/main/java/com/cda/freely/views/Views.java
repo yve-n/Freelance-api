598563115c9e1a36cd455c4edd1af105fa718fd1
yve-n <yve.nguetche@gmail.com>
2023-05-24 23:41:19 +0200

    set up views to prevent recursivity

--- src/main/java/com/cda/freely/views/Views.java
+++ src/main/java/com/cda/freely/views/Views.java
@@ -1,10 +1,21 @@
 package com.cda.freely.views;
 
 public class Views {
-        public interface TagWithUsersView{}
-        public interface UserDetails {}
-        public interface UserDetailsPlus extends UserDetails {}
-        public interface UserSummary {}
+        public static class User{}
+        public static class Company{}
+        public static class Address{}
+        public static class Family{}
+        public static class Tag{}
+        public static class Experience{}
+        public static class Skill{}
+        public static class Service{}
+        public static class Image{}
+        public static class Achievement{}
+        public static class Training{}
+        public static class History{}
+        public static class Contact{}
+        public static class AthenticationResponse extends User{}
+
 
 
 }
