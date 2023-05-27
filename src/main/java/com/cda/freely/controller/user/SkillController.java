7492db83252383bf497d9f4bc38f2ef6ba5aebd9
yve-n <yve.nguetche@gmail.com>
2023-05-27 14:33:14 +0200

    add skillController

--- src/main/java/com/cda/freely/controller/user/SkillController.java
+++ src/main/java/com/cda/freely/controller/user/SkillController.java
@@ -2,6 +2,7 @@ package com.cda.freely.controller.user;
 
 import com.cda.freely.entity.Skill;
 import com.cda.freely.exception.GlobalExceptionHandler;
+import com.cda.freely.service.SkillService;
 import com.cda.freely.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,23 +12,25 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/user/profile")
+@RequestMapping("/user/skills")
 @RequiredArgsConstructor
 public class SkillController {
     private UserService userService;
+    private SkillService skillService;
     private final Logger logger = LoggerFactory.getLogger(SkillController.class);
     @Autowired
-    public SkillController(UserService userService) {
+    public SkillController(UserService userService, SkillService skillService) {
         this.userService = userService;
+        this.skillService = skillService;
     }
-    @PostMapping("/skill")
+    @PostMapping("")
     public ResponseEntity<?> addSkill(@RequestHeader("Authorization") String bearerToken,
                                         @RequestBody Skill skill){
         try{
-            System.out.println(bearerToken);
-            logger.info("Add a new skill-----------: {}",skill);
-            Skill newSkill = new Skill();
-            return ResponseEntity.ok(skill);
+            logger.warn("bearer token ---------- {}", bearerToken);
+            logger.info("Add a new skill-----------: {}",skill.toString());
+            Skill newSkill = skillService.saveSkill(skill);
+            return ResponseEntity.ok(newSkill);
         }catch (Exception e){
             return new GlobalExceptionHandler().handleAllExceptions(e);
         }
