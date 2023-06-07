87d7baea26d78328cf33cd468dfc5c63e898727b
yve-n <yve.nguetche@gmail.com>
2023-06-08 00:39:14 +0200

    optimize import

--- src/main/java/com/cda/freely/controller/admin/AdminController.java
+++ src/main/java/com/cda/freely/controller/admin/AdminController.java
@@ -1,20 +1,16 @@
 package com.cda.freely.controller.admin;
 
-import com.cda.freely.entity.User;
 import com.cda.freely.service.admin.AdminService;
 import com.cda.freely.views.Views;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
