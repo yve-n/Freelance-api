dee3dcb8c7fc7c8d549a4bb757ef842611b1727f
yve-n <yve.nguetche@gmail.com>
2023-06-21 05:35:05 +0200

    add logout implementation

--- src/main/java/com/cda/freely/controller/user/UserController.java
+++ src/main/java/com/cda/freely/controller/user/UserController.java
@@ -27,43 +27,17 @@ public class UserController {
         this.authService = authService;
     }
 
-    @GetMapping("")
-    public String HelloUser(@RequestHeader("Authorization") String bearerToken){
-        System.out.println(bearerToken);
-        return "hello user";
-    }
-    @GetMapping("/test")
-    public String testUser(@RequestHeader("Authorization") String bearerToken){
-        System.out.println(bearerToken);
-        return "test user";
-    }
-
     @PostMapping("/profile")
-    public ResponseEntity<?> getUser(@RequestBody User user){
-        try{
-            Optional<User> foundUser = userService.findById(user.getId());
-            if (foundUser.isPresent()){
-                return ResponseEntity.ok(foundUser);
-            }else{
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found");
-            }
-
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<?> getUser(@RequestHeader("Authorization") String bearerToken){
+        User user =  userService.checkUser(bearerToken);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<String> logout(@RequestHeader("Authorization") String jwt) {
-        String token = jwt.substring(7);
-        Long userId = (Long) tokenProvider.getTokenBody(token).get("id");
-
-        Optional<User> user = authService.findById(userId);
-        if (user.isPresent()) {
-            user.get().setTokenNumber(user.get().getTokenNumber() + 1);
-            authService.saveUser(user.get());
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<String> logout(@RequestHeader("Authorization") String bearerToken) {
+        User user =  userService.checkUser(bearerToken);
+        user.setTokenNumber(user.getTokenNumber() + 1);
+        userService.saveUser(user);
+        return ResponseEntity.ok().build();
     }
 }
